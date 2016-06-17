3e2cf595710f632dceaf8fe826f6d78016e682a4
José Ortega <jose.ortega.soto@gmail.com>
2016-06-17 13:10:45 -0400

    creando directorios para ordenar la aplicación

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'my-app',
-	templateUrl: 'app/app.html'
+	templateUrl: 'app/components/app.html'
 })
 
 export class AppComponent {
